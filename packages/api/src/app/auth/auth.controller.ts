f6211195849ef30f1583832fd7c43c5dd7565959
Jakub Szwajka <szwajkajakub@gmail.com>
2023-05-21 11:01:00 +0200

    feat: expose login endpoint as public

--- packages/api/src/app/auth/auth.controller.ts
+++ packages/api/src/app/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
+import { Public } from '../public.decorator';
 
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
