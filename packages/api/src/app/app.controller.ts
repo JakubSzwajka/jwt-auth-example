56bc24f5e274aa0ac97fc25abc21f82d7b680ecb
Jakub Szwajka <szwajkajakub@gmail.com>
2023-05-21 10:33:48 +0200

    feat: init local strategy

--- packages/api/src/app/app.controller.ts
+++ packages/api/src/app/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -9,5 +10,11 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
   }
 }
