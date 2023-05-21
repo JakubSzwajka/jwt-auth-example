7d94f34b2d43c392f12c9bc3ff471e380a5419fd
Jakub Szwajka <szwajkajakub@gmail.com>
2023-05-21 10:52:36 +0200

    feat: enable auth globally

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    }
+  ],
 })
 export class AppModule {}
--- packages/api/src/app/users/users.controller.ts
+++ packages/api/src/app/users/users.controller.ts
@@ -1,10 +1,8 @@
-import { Controller, Get, Request, UseGuards } from "@nestjs/common";
-import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
+import { Controller, Get, Request} from "@nestjs/common";
 
 @Controller('users')
 export class UsersController {
 
-    @UseGuards(JwtAuthGuard)
     @Get('profile')
     getProfile(@Request() req) {
         return req.user;
